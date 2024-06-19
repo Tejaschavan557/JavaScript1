a0b2774b6ed3edb5e161ea383cf5328e07e60eae
Tejaschavan557 <137384682+Tejaschavan557@users.noreply.github.com>
2024-06-19 17:47:28 +0000

    data types summry

---
+++ .devcontainer/Basics-01/comparsion.js
@@ -0,0 +1,16 @@
+// console.log(2>1);
+// console.log(2!=1);
+// console.log(2==1);
+// console.log(2<1);
+
+//comparison with different data type
+
+console.log("1"==2);
+console.log("2">=1);
+
+
+//with null values
+
+console.log(null >1);
+console.log(null >=1);
+console.log(null <=0);
---
+++ .devcontainer/Basics-01/datatypes_2.js
@@ -0,0 +1,24 @@
+// data types
+
+//primitive dat types
+// 7 types : String , number , Boolean , null, undefines, symbol, Bigint
+
+
+
+const num = 1234;
+const string ="tejas";
+const status =false;
+const salary = null;
+let  designation;
+const bignum =12222222222222222222222222222222222222n
+
+// non primitive data types
+// arrays , object , functions
+
+const arr =["amar","akbar", "anthony"];
+
+const obj ={name:"tejas", age: 19, Des: "SDE"};
+
+const myfunc = function(){
+console.log("hello world");
+}
